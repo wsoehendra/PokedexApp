db017d3adb30c092883c1582fb2a0ba85fe9a6a2
Will Soehendra <wsoehendra@gmail.com>
2016-09-18 23:41:07 +1000

    Initial Commit2

--- app/src/main/java/com/will/android/pokedexapp/pokedexChooser.java
+++ app/src/main/java/com/will/android/pokedexapp/pokedexChooser.java
@@ -1,23 +1,18 @@
 package com.will.android.pokedexapp;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
-
-    ImageButton openBtn;
+public class pokedexChooser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +20,7 @@ public class FullscreenActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().SYSTEM_UI_FLAG_FULLSCREEN);
         actionBar.hide();
-        setContentView(R.layout.activity_fullscreen);
-        openBtn = (ImageButton) findViewById(R.id.openBtn);
-        openBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToChooser();
-            }
-        });
-    }
-
-    public void goToChooser(){
-        Intent intent = new Intent(this, pokedexChooser.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_pokedex_chooser);
     }
 
 }
